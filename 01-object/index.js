964993b6a76c7737f944434c7b4fc355a5516774
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-06-26 22:26:07 +0530

    objects

---
+++ 01-object/index.js
@@ -0,0 +1,13 @@
+// Create an object called person with properties name, age, and gender.
+// Access and log the name property to the console.
+
+
+const person={
+    name:"Rajan",
+    age:22,
+    gender:"male"
+}
+
+console.log(person.name)
+console.log(person.age);
+console.log(person.gender)
---
+++ 01-object/index1.js
@@ -0,0 +1,12 @@
+// Create an object called student with properties: name, grade, and subject. Access
+// and log the grade property to the console.
+
+const student={
+    name:"Rajan",
+    grade:"A",
+    subject:"Coding"
+}
+
+console.log(student.name)
+console.log(student.grade)
+console.log(student.subject)
