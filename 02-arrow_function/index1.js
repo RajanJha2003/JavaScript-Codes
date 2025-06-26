4bfdac4d186b2140d9143125e60bb6c90413873c
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-06-26 22:31:05 +0530

    arrow function

---
+++ 02-arrow_function/index1.js
@@ -0,0 +1,12 @@
+// Create an arrow function named calculateGrade that takes a score as a parameter and returns the grade (e.g., A, B, C).
+
+
+const calculateGrade= (score) => {
+    if(score>90) return 'A'
+    else if(score>75) return 'B'
+    else if(score>60) return 'C'
+    else if(score>35) return 'D'
+    else return 'F'
+}
+
+console.log(calculateGrade(78))
